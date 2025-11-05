e2ab6101ca9fc68553fdd5596654f90b9f3f9a6e
I-AM-VLAD <vlad1slaw19982@gmail.com>
2025-11-05 18:31:49 +0200

    Доменная модель [#505091 #551234]

--- src/main/java/ru/job4j/smapi/Job4jSocialMediaApiApplication.java
+++ src/main/java/ru/job4j/smapi/Job4jSocialMediaApiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Job4jSocialMediaApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Job4jSocialMediaApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jSocialMediaApiApplication.class, args);
+    }
 }
